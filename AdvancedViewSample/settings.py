cff3bc1fad35ca873e98c20d8a3c2fbade7f0c49
yuki1582 <yuki.nakamura1103@gmail.com>
2023-03-21 15:31:32 +0900

    login_auth_add

--- AdvancedViewSample/settings.py
+++ AdvancedViewSample/settings.py
@@ -110,6 +110,9 @@ AUTH_PASSWORD_VALIDATORS = [
     {
         'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
     },
+    {
+        'NAME': 'utils.validators.CustomPasswordValidator'
+    }
 ]
 
 
